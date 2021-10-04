8359fdf7566ca821ae6fa840635e8f529e954744
Sqvall <masster1987@inbox.ru>
2021-10-04 13:06:17 +0300

    Add filter for entries by feed id and tags id.

--- src/app/db/repositories/entries.py
+++ src/app/db/repositories/entries.py
@@ -2,14 +2,16 @@ from __future__ import annotations
 
 from typing import List
 
-from sqlalchemy import func, desc
+from sqlalchemy import func, desc, distinct
 from sqlalchemy.engine import ChunkedIteratorResult
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
+from sqlalchemy.sql import Select
 
 from app.db.errors import EntityDoesNotExist
 from app.db.repositories.base import BaseRepository
+from app.models import Tag
 from app.models.entries import Entry
 
 
@@ -28,12 +30,26 @@ class EntriesRepository(BaseRepository):
 
         return [entry.id for entry in entries]
 
-    async def get_entries(self, *, limit: int = 20, offset: int = 0, ) -> List[Entry]:
-        stmt = (select(self.model)
-                .order_by(desc(self.model.updated_at))
-                .offset(offset)
-                .limit(limit)
-                .options(selectinload(self.model.tags), selectinload(self.model.feed)))
+    async def list_entries(
+            self,
+            *,
+            tag_ids: List[int] = None,
+            feed_id: int = None,
+            limit: int = 20,
+            offset: int = 0,
+    ) -> List[Entry]:
+        stmt: Select = (select(self.model)
+                        .order_by(desc(self.model.updated_at))
+                        .offset(offset)
+                        .limit(limit)
+                        .options(selectinload(self.model.tags), selectinload(self.model.feed))
+                        .distinct())
+
+        if tag_ids:
+            stmt = stmt.join(self.model.tags).where(Tag.id.in_(tag_ids))  # noqa
+
+        if feed_id:
+            stmt = stmt.where(self.model.feed_id == feed_id)
 
         result: ChunkedIteratorResult = await self._session.execute(stmt)
 
